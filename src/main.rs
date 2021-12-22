a7eec450e36a80fa0f97994e4fdc5af4ede239d5
valkyrie_pilot <valk@ferris.chat>
2021-12-21 20:07:02 -0700

    updoot

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use text_io::read;
-
 fn main() {
-    println!("Enter number to run collatz conjecture on: ");
-    let mut i: i64 = read!();
-    while i != 1 {
-        if i % 2 != 0 {
-            i = i * 3 + 1
-        } else {
-            i = i / 2
+    println!("Running Collatz conjecture...");
+    let args: Vec<String> = std::env::args().collect();
+    let mut first_arg: u64 = args[1].parse().unwrap();
+    let mut to_check: u64 = 1;
+    if args.len() > 2 {
+        println!("`{}`", args[2]);
+        if args[2] == "--prove" {
+            while first_arg >= to_check {
+                let mut checking: u64 = to_check;
+                while checking != 1 {
+                    if checking % 2 != 0 {
+                        checking = checking * 3 + 1
+                    } else {
+                        checking = checking / 2
+                    }
+                }
+                println!("{}", checking);
+                to_check += 1;
+            }
         }
-        println!("{0}", i)
+    }
+    while first_arg != 1 {
+        if first_arg % 2 != 0 {
+            first_arg = first_arg * 3 + 1
+        } else {
+            first_arg = first_arg / 2
+        }
+        println!("{0}", first_arg)
     }
 }
